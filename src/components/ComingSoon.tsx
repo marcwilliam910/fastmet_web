505ff43a4d870a84be34e3568b7f49fee5e10743
marcwilliam910 <marcwilliamvaliente910@gmail.com>
2025-10-01 09:32:54 +0800

    make coming soon text bigger

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -4,7 +4,7 @@ export default function ComingSoon() {
   return (
     <div className="flex flex-col gap-5 items-center lg:flex-1 lg:gap-10">
       <img src={logo} alt="FastMet Logo" className="w-32 lg:w-48 xl:w-56" />
-      <h1 className="text-3xl md:text-4xl font-bold text-[#FFA840]">
+      <h1 className="text-3xl md:text-4xl font-bold text-[#FFA840] lg:text-6xl">
         Coming Soon!
       </h1>
     </div>
